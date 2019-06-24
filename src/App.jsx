b2ab7c32cff53dc5a87d261e923d61b52cf2ae48
Simon Holmes <srsholmes@gmail.com>
2019-06-24 20:35:52 +0100

    changed loader and setup useEffect hook as prep for the github API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useDebounce} from './hooks/debounce';
 import {Loader} from './components/loader';
 
 const App = () => {
-  const [searchInput, changeInput] = useState('');
+  const [searchInput, setSearchInput] = useState('');
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const debouncedSearchTerm = useDebounce(searchInput, 500);
-  console.log({debouncedSearchTerm});
-  const setSearchInput = (e) => {
-    changeInput(e.target.value);
-  };
+  useEffect(
+    () => {
+      if (debouncedSearchTerm) {
+        setIsLoading(true);
+      } else {
+        setResults([]);
+      }
+    },
+    [debouncedSearchTerm]
+  );
   return (
     <>
       <h3>Please search for a Github User:</h3>
-      <input type="text" value={searchInput} onChange={setSearchInput}/>
-      {searchInput.length > 0 && results.length === 0 && (
+      <input
+        type="text"
+        value={searchInput}
+        onChange={e => setSearchInput(e.target.value)}
+      />
+      {isLoading && (
         <Loader/>
       )}
     </>
