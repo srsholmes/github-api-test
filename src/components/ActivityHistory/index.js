a3b3bf775aa1ef371ef1c7585ba06448d9bb9ae7
Simon Holmes <srsholmes@gmail.com>
2019-06-25 16:36:57 +0100

    make commit dates human readable

--- src/components/ActivityHistory/index.js
+++ src/components/ActivityHistory/index.js
@@ -11,7 +11,7 @@ export const ActivityHistory = ({activity, username}) => {
   }
   return (
     <>
-      <h4 className={styles.title}>Recent commit activity:</h4>
+      <h4 className={styles.title}>Commit activity:</h4>
       <ul className={styles.activityWrapper}>
         {activity.length > 0
           && activity.map((x, i) => {
@@ -25,14 +25,14 @@ export const ActivityHistory = ({activity, username}) => {
                     {x.repository.name}
                   </a>
                 </p>
-                <p>Commit History Times:</p>
+                <p>Recent Commit History Dates:</p>
                 <ul className={styles.commitsWrapper}>
                   {x.contributions.edges.length > 0
                     && x.contributions.edges.map(
                       ({node: {occurredAt, url}}, j) => (
                         <li className={styles.commitTime} key={j}>
                           <a target="_blank" href={url}>
-                            {occurredAt}
+                            {new Date(`${occurredAt}`).toDateString()}
                           </a>
                         </li>
                       )
