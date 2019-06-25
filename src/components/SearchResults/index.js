14ae8a683d954571c7db87ccdb8cbcb6bf39ab48
Simon Holmes <srsholmes@gmail.com>
2019-06-25 02:19:29 +0100

    more css

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -22,28 +22,30 @@ export const SearchResults = ({results}) => {
       {results.length ? (
         results.map(({node: {name, avatarUrl, login}}) => (
           <li key={avatarUrl} className={styles.result}>
-            <img className={styles.avatar} src={avatarUrl} alt={name} />
-            <div className={styles.infoWrapper}>
-              <div className={styles.info}>
-                <span className={styles.title}>{'Name:'}</span>
-                <span>{name}</span>
+            <>
+              <img className={styles.avatar} src={avatarUrl} alt={name} />
+              <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                  <span className={styles.title}>{'Name:'}</span>
+                  <span>{name}</span>
+                </div>
+                <div className={styles.info}>
+                  <span className={styles.title}>{'Username:'}</span>
+                  <span>{login}</span>
+                </div>
+                {!userActivity[login] && (
+                  <button onClick={() => setSelectedUser(login)}>
+                    Get User Activity
+                  </button>
+                )}
               </div>
-              <div className={styles.info}>
-                <span className={styles.title}>{'Username:'}</span>
-                <span>{login}</span>
-              </div>
-              {!userActivity[login] && (
-                <button onClick={() => setSelectedUser(login)}>
-                  Get User Activity
-                </button>
-              )}
               {userActivity[login] && (
                 <ActivityHistory
                   username={login}
                   activity={userActivity[login]}
                 />
               )}
-            </div>
+            </>
           </li>
         ))
       ) : (
