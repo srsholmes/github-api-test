59642c9dadc5e581dafddec68c497370437ff1b4
Simon Holmes <srsholmes@gmail.com>
2019-06-25 02:06:49 +0100

    css for activity results

--- src/components/ActivityHistory/index.js
+++ src/components/ActivityHistory/index.js
@@ -5,20 +5,18 @@ export const ActivityHistory = ({activity}) => {
   return (
     <>
       <h4 className={styles.title}>Recent commit activity:</h4>
-      <ul>
-        {activity.map((x, i) => {
+      <ul className={styles.activityWrapper}>
+        {activity.map((x) => {
           return (
-            <>
-              <li className={styles.repo} key={i}>
-                <p className={styles.repoName}>{x.repository.name}</p>
-              </li>
+            <li className={styles.repo}>
+              <p className={styles.repoName}>{x.repository.name}</p>
               <p>Commit Times:</p>
               <ul>
                 {x.contributions.edges.map(({node: {occurredAt}}, j) => (
                   <li key={j}>{occurredAt}</li>
                 ))}
               </ul>
-            </>
+            </li>
           );
         })}
       </ul>
