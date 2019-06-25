f7af46cc467125a2191ecab49d0061a8da1f24af
Simon Holmes <srsholmes@gmail.com>
2019-06-25 17:08:09 +0100

    loader story book

---
+++ stories/Loader.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {Loader} from '../src/components/Loader';
+
+storiesOf('Loader', module).add('Loading', () => <Loader />);
