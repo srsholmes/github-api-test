da576bc92c5f4b19d2ae88520aaf6517af4af0e7
Simon Holmes <srsholmes@gmail.com>
2019-06-25 09:48:08 +0100

    additional checks

--- src/components/ActivityHistory/index.js
+++ src/components/ActivityHistory/index.js
@@ -2,34 +2,41 @@ import React from 'react';
 import styles from './styles.scss';
 
 export const ActivityHistory = ({activity, username}) => {
+  if (activity.length === 0) {
+    return <h4 className={styles.title}>No Activity Found</h4>;
+  }
   return (
     <>
       <h4 className={styles.title}>Recent commit activity:</h4>
       <ul className={styles.activityWrapper}>
-        {activity.map(x => {
-          return (
-            <li className={styles.repo}>
-              <p className={styles.repoName}>
-                <a
-                  target={'_blank'}
-                  href={`http://github.com/${username}/${x.repository.name}`}
-                >
-                  {x.repository.name}
-                </a>
-              </p>
-              <p>Commit History Times:</p>
-              <ul className={styles.commitsWrapper}>
-                {x.contributions.edges.map(
-                  ({node: {occurredAt, url}}, j) => (
-                    <li className={styles.commitTime} key={j}>
-                      <a target="_blank" href={url}>{occurredAt}</a>
-                    </li>
-                  )
-                )}
-              </ul>
-            </li>
-          );
-        })}
+        {activity.length > 0
+          && activity.map(x => {
+            return (
+              <li className={styles.repo}>
+                <p className={styles.repoName}>
+                  <a
+                    target={'_blank'}
+                    href={`http://github.com/${username}/${x.repository.name}`}
+                  >
+                    {x.repository.name}
+                  </a>
+                </p>
+                <p>Commit History Times:</p>
+                <ul className={styles.commitsWrapper}>
+                  {x.contributions.edges.length > 0
+                    && x.contributions.edges.map(
+                      ({node: {occurredAt, url}}, j) => (
+                        <li className={styles.commitTime} key={j}>
+                          <a target="_blank" href={url}>
+                            {occurredAt}
+                          </a>
+                        </li>
+                      )
+                    )}
+                </ul>
+              </li>
+            );
+          })}
       </ul>
     </>
   );
